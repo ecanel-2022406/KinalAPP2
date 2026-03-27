fdeb7af43593fde00324ce770d2cb603feb724d8
ecanel-2022406 <ecanel-2022406@kinal.edu.gt>
2026-03-26 22:31:13 -0600

    refactor: Se agrego una capa de servicio y una interfaz de ventas ecanel-2022406

---
+++ src/main/java/com/edycanel/Kinalapp/service/IVentaService.java
@@ -0,0 +1,27 @@
+package com.edycanel.Kinalapp.service;
+
+import com.edycanel.Kinalapp.entity.Venta;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+import java.util.List;
+
+public interface IVentaService {
+
+    @Transactional(readOnly = true)
+    List<Venta> listarTodos();
+
+    Venta guardar(Venta venta);
+
+    Optional<Venta> buscarPorCodigoVenta(int codigoVenta);
+
+    @Transactional(readOnly = true)
+    List<Venta> buscarPorEstado(int estado);
+
+    Venta actualizar(int codigoVenta, Venta venta);
+
+    void eliminar(int codigoVenta);
+
+    boolean existePorCodigoVenta(int codigoVenta);
+}
